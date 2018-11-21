d130ea57fdabf7c23e5dad4432c192a69f75306f
Luis Revilla <luisrevillameza@gmail.com>
2018-11-21 14:22:00 -0500

    formatting App.js with prettier

--- src/App.js
+++ src/App.js
@@ -1,34 +1,35 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
-    return (<div className="Senior">
-      <div className="App">
-<div className="Top"></div>
-<div className="F2"></div>
-<div className="F3"></div>
-<div className="F4"></div>
-<div className="F5"></div>
-      <div className="Left">
-</div>
-        <div className="Middle">
-        <div className="F1"></div>
-          <div className="triangle-bottomleft"></div>
-          <div className="triangle-bottomright"></div>
+    return (
+      <div className="Senior">
+        <div className="App">
+          <div className="Top" />
+          <div className="F2" />
+          <div className="F3" />
+          <div className="F4" />
+          <div className="F5" />
+          <div className="Left" />
+          <div className="Middle">
+            <div className="F1" />
+            <div className="triangle-bottomleft" />
+            <div className="triangle-bottomright" />
+          </div>
 
+          <div className="Right"> </div>
 
-
-</div>
-
-<div className="Right"> </div>
-
-<div className="MainText"><h1 className="Title">Luis Revilla</h1><div className="SubTitle">Software Engineer based in New York</div><div className="SubTitle"> +3 years of JavaScript experience</div> </div>
-
-
-
-
-      </div></div>
+          <div className="MainText">
+            <h1 className="Title">Luis Revilla</h1>
+            <div className="SubTitle">Software Engineer based in New York</div>
+            <div className="SubTitle">
+              {" "}
+              +3 years of JavaScript experience
+            </div>{" "}
+          </div>
+        </div>
+      </div>
     );
   }
 }
