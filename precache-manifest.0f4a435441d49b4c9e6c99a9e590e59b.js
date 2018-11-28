11af9aa65da69a7c5a52ef81953f0b532ad6df96
Luis Revilla <luisrevillameza@gmail.com>
2018-11-28 17:18:08 -0500

    New local commit

---
+++ precache-manifest.0f4a435441d49b4c9e6c99a9e590e59b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "d46c63e0c40d1afd590132d8dae978f4",
+    "url": "/static/media/design.d46c63e0.svg"
+  },
+  {
+    "revision": "4715afa3226e735b8825",
+    "url": "/static/css/main.0091a87e.chunk.css"
+  },
+  {
+    "revision": "af67e2650c7a8c8d9b34",
+    "url": "/static/js/1.af67e265.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9ffffa509585398b6bffed94325f9d55",
+    "url": "/static/media/plat.9ffffa50.svg"
+  },
+  {
+    "revision": "1880767ce9dea853aaf28a89a02a5f03",
+    "url": "/static/media/react.1880767c.svg"
+  },
+  {
+    "revision": "4715afa3226e735b8825",
+    "url": "/static/js/main.4715afa3.chunk.js"
+  },
+  {
+    "revision": "b4ca3c8016295ee9ddc14492cffdc2e1",
+    "url": "/static/media/dialogue.b4ca3c80.svg"
+  },
+  {
+    "revision": "db7880e0eb8127dac95133a6189deb3a",
+    "url": "/static/media/learn.db7880e0.svg"
+  },
+  {
+    "revision": "2222196b0e700d915239190241b653e0",
+    "url": "/static/media/backend.2222196b.svg"
+  },
+  {
+    "revision": "8e866edf0281d5054e5a674eb8e73671",
+    "url": "/static/media/amore.8e866edf.gif"
+  },
+  {
+    "revision": "d4212a5a44190e13b27715a2b6f325b1",
+    "url": "/static/media/ny.d4212a5a.jpg"
+  },
+  {
+    "revision": "32e6c74fe9f520ac8a2df46d450a9f5d",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.18c270ded71decc0e86e7e2d2d5cbbae.js"
+  "/precache-manifest.0f4a435441d49b4c9e6c99a9e590e59b.js"
 );
 
 workbox.clientsClaim();
