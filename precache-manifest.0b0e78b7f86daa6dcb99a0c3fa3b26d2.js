bd3ba67a619de17b886ea5dfcff3d0938e3eead1
Luis Revilla <luisrevillameza@gmail.com>
2020-04-23 16:56:19 -0400

    New local commit

--- precache-manifest.0b0e78b7f86daa6dcb99a0c3fa3b26d2.js
+++ precache-manifest.0b0e78b7f86daa6dcb99a0c3fa3b26d2.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "9b57daa520acce3e1ed9",
+    "revision": "6ba4f3c9a544cd41b3eb",
     "url": "/static/css/main.2dc84c7a.chunk.css"
   },
   {
-    "revision": "9b57daa520acce3e1ed9",
-    "url": "/static/js/main.9b57daa5.chunk.js"
+    "revision": "6ba4f3c9a544cd41b3eb",
+    "url": "/static/js/main.6ba4f3c9.chunk.js"
   },
   {
     "revision": "e614a9c1ee9bb54d1d84",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "cddf0f8613d4c8f52eb8a84863659c18",
+    "revision": "f9bfe91e0177827174fe0a948485bc40",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d073f533187ea4d328fa05aeb515af37.js"
+  "/precache-manifest.0b0e78b7f86daa6dcb99a0c3fa3b26d2.js"
 );
 
 workbox.clientsClaim();
