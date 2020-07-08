089c70e5d76206e2e8f86bde09225a818756af00
Luis Revilla <luisrevillameza@gmail.com>
2020-07-08 15:15:23 -0400

    New local commit

--- precache-manifest.caaebb2f567d41d128dc7acd502c98ad.js
+++ precache-manifest.caaebb2f567d41d128dc7acd502c98ad.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "13a2c1e93a01466647ac",
+    "revision": "cbf58fead74eb7955d32",
     "url": "/static/css/main.2dc84c7a.chunk.css"
   },
   {
-    "revision": "13a2c1e93a01466647ac",
-    "url": "/static/js/main.13a2c1e9.chunk.js"
+    "revision": "cbf58fead74eb7955d32",
+    "url": "/static/js/main.cbf58fea.chunk.js"
   },
   {
     "revision": "e614a9c1ee9bb54d1d84",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "1c0b6764175c361b64d464ebb25267c5",
+    "revision": "61cd2c35e8e52fcb75a816825ee62f56",
     "url": "/index.html"
   }
 ];
